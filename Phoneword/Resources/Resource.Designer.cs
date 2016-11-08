a7b2ac7e413da9e53df0a8f2f3abaf9224652c16
Linhy.Augur <linhy@augurit.com>
2016-11-08 15:23:19 +0800

    fix btn id

--- Phoneword/Resources/Resource.Designer.cs
+++ Phoneword/Resources/Resource.Designer.cs
@@ -60,20 +60,20 @@ namespace Phoneword
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050004
-			public const int CallButton = 2131034116;
-			
-			// aapt resource value: 0x7f050000
-			public const int CallHistoryButton = 2131034112;
-			
-			// aapt resource value: 0x7f050002
-			public const int PhoneNumberText = 2131034114;
-			
 			// aapt resource value: 0x7f050003
-			public const int TranslateButton = 2131034115;
+			public const int CallButton = 2131034115;
+			
+			// aapt resource value: 0x7f050004
+			public const int CallHistoryButton = 2131034116;
 			
 			// aapt resource value: 0x7f050001
-			public const int textView1 = 2131034113;
+			public const int PhoneNumberText = 2131034113;
+			
+			// aapt resource value: 0x7f050002
+			public const int TranslateButton = 2131034114;
+			
+			// aapt resource value: 0x7f050000
+			public const int textView1 = 2131034112;
 			
 			static Id()
 			{
